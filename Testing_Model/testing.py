a8a38a17119340992f75f29f73330d4789af7bd3
bluevita <bluevitagaming@gmail.com>
2023-12-19 00:30:29 +0700

    memperbaharui file testing model

--- Testing_Model/testing.py
+++ Testing_Model/testing.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-#from keras.preprocessing import image
 import numpy as np
 import keras.utils as image
 
@@ -12,13 +11,21 @@ def preprocess_image(img_path):
     img_array /= 255.0
     return img_array
 
-image_path = "D:/Project/capstone/gambar/baru.jpg"
+image_path = "image/baru.jpg"
 
 preprocessed_image = preprocess_image(image_path)
 
 predictions = model.predict(preprocessed_image)
 
-class_labels = ['Biru', 'Coklat', 'Hijau', 'Hitam', 'Jingga', 'Kuning', 'Merah', 'Putih']
+class_labels = ["Biru, Warna biru pada urine tidak umum dan mungkin merupakan hasil dari konsumsi pewarna makanan tertentu. Namun, jika warna biru terus menerus muncul, itu bisa menunjukkan masalah dengan ginjal atau saluran kemih. Disarankan untuk berkonsultasi dengan profesional kesehatan.",
+                "Coklat, Warna coklat pada urine bisa disebabkan oleh dehidrasi, konsumsi makanan tertentu, atau masalah dengan hati. Tetapi, coklat juga dapat menjadi tanda adanya darah dalam urine, yang memerlukan perhatian medis segera.",
+                "Hijau, Warna hijau pada urine biasanya disebabkan oleh konsumsi makanan atau suplemen tertentu. Namun, jika hijau terus muncul dan tidak dapat dijelaskan oleh faktor diet, itu bisa mengindikasikan masalah kesehatan seperti infeksi atau penyakit hati. Konsultasikan dengan dokter untuk evaluasi lebih lanjut.",
+                "Hitam, Urine hitam dapat menjadi tanda adanya darah teroksidasi atau masalah hati. Beberapa jenis makanan dan obat-obatan juga dapat menyebabkan urine hitam. Jika urine hitam terus muncul, segera hubungi profesional kesehatan.",
+                "Jingga, Warna jingga pada urine mungkin disebabkan oleh dehidrasi atau konsumsi vitamin B kompleks. Namun, jika jingga terus muncul tanpa alasan yang jelas, itu bisa menunjukkan masalah dengan hati atau saluran empedu. Periksakan ke dokter untuk evaluasi lebih lanjut.",
+                "Kuning, Warna kuning pada urine biasanya normal dan disebabkan oleh pigmen urine yang disebut urobilin. Namun, warna kuning yang sangat gelap atau kuning kecoklatan bisa menunjukkan dehidrasi. Pastikan untuk cukup minum air.",
+                "Merah, Urine merah dapat disebabkan oleh keberadaan darah. Ini bisa disebabkan oleh infeksi saluran kemih, batu ginjal, atau masalah lain pada ginjal atau kandung kemih. Segera cari bantuan medis jika urine berwarna merah.",
+                "Putih, Urine yang putih atau bening (transparan) umumnya dianggap normal dan sehat. Ini menunjukkan bahwa Anda mungkin sedang minum cukup air, dan ginjal Anda berfungsi dengan baik untuk menyaring kelebihan air dalam tubuh. Peningkatan konsumsi air dapat menghasilkan urine yang lebih transparan."
+                ]
 
 confidence_score = predictions.max() * 100
 
